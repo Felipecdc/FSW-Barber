9ccde0dd9047bde214c2a86fd12a13788558a108
Felipe Castro <felipecdc09@gmail.com>
2024-05-08 20:45:05 -0300

    feat: add initial header component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,9 @@
-import About from "./about.page";
+import Header from "../_components/header";
 
 export default function Home() {
   return (
     <div>
-      <About />
+      <Header />
     </div>
   );
 }
